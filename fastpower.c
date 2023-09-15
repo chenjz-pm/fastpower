8edda442cb132f35129b4d3b8c3b5be385c7f561
chenjz-pm <chenjz.pm@gmail.com>
2023-09-15 13:31:29 +0800

    build 3

--- fastpower.c
+++ fastpower.c
@@ -1,37 +1,20 @@
 #include "ntapi.h"
+#include "opt.h"
 
-void power(int argc, char* argv[])
+void power(int power_action,int sleep)
 {
-	if (argc == 1)
-	{
-		NtInitiatePowerAction(PowerActionShutdown, PowerSystemShutdown, 0, TRUE);
-	}
+	Sleep(sleep);
 
-	else if (stricmp(argv[1], "sleep") == 0 || stricmp(argv[1], "sl") == 0)
+	if (power_action == 2)
 	{
-		NtInitiatePowerAction(PowerActionSleep, PowerSystemSleeping1, 0, TRUE);
-		NtInitiatePowerAction(PowerActionSleep, PowerSystemSleeping2, 0, TRUE);
-		NtInitiatePowerAction(PowerActionSleep, PowerSystemSleeping3, 0, TRUE);
+		for (int i = 2; i < 5; i++)
+		{
+			NtInitiatePowerAction(PowerActionSleep, i, 0, TRUE);
+		}
 	}
-
-	else if (stricmp(argv[1], "hibernate") == 0 || stricmp(argv[1], "hi") == 0)
+	else
 	{
-		NtInitiatePowerAction(PowerActionHibernate, PowerSystemHibernate, 0, TRUE);
-	}
-
-	else if (stricmp(argv[1], "shutdown") == 0 || stricmp(argv[1], "sh") == 0)
-	{
-		NtInitiatePowerAction(PowerActionShutdown, PowerSystemShutdown, 0, TRUE);
-	}
-
-	else if (stricmp(argv[1], "reboot") == 0 || stricmp(argv[1], "re") == 0)
-	{
-		NtInitiatePowerAction(PowerActionShutdownReset, PowerSystemWorking, 0, TRUE);
-	}
-
-	else if (stricmp(argv[1], "shutdownoff") == 0 || stricmp(argv[1], "so") == 0)
-	{
-		NtInitiatePowerAction(PowerActionShutdownOff, PowerSystemShutdown, 0, TRUE);
+		NtInitiatePowerAction(power_action, NIPA_arg2(power_action), 0, TRUE);
 	}
 }
 
@@ -39,7 +22,9 @@ int main(int argc, char* argv[])
 {
 	GetPrivilege(SE_SHUTDOWN_NAME);
 
-	power(argc, argv);
+	paropt(argc, argv);
+
+	power(reco1,reco2);
 
 	return 0;
 }
