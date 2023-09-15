3ea9ce1bf37b2cbdd7abf4b92aba508b33db6c03
chenjz-pm <chenjz.pm@gmail.com>
2023-09-15 14:29:10 +0800

    Revert "build 3"
    This reverts commit 8edda442cb132f35129b4d3b8c3b5be385c7f561.

--- fastpower.c
+++ fastpower.c
@@ -1,20 +1,37 @@
 #include "ntapi.h"
-#include "opt.h"
 
-void power(int power_action,int sleep)
+void power(int argc, char* argv[])
 {
-	Sleep(sleep);
-
-	if (power_action == 2)
+	if (argc == 1)
 	{
-		for (int i = 2; i < 5; i++)
-		{
-			NtInitiatePowerAction(PowerActionSleep, i, 0, TRUE);
-		}
+		NtInitiatePowerAction(PowerActionShutdown, PowerSystemShutdown, 0, TRUE);
 	}
-	else
+
+	else if (stricmp(argv[1], "sleep") == 0 || stricmp(argv[1], "sl") == 0)
 	{
-		NtInitiatePowerAction(power_action, NIPA_arg2(power_action), 0, TRUE);
+		NtInitiatePowerAction(PowerActionSleep, PowerSystemSleeping1, 0, TRUE);
+		NtInitiatePowerAction(PowerActionSleep, PowerSystemSleeping2, 0, TRUE);
+		NtInitiatePowerAction(PowerActionSleep, PowerSystemSleeping3, 0, TRUE);
+	}
+
+	else if (stricmp(argv[1], "hibernate") == 0 || stricmp(argv[1], "hi") == 0)
+	{
+		NtInitiatePowerAction(PowerActionHibernate, PowerSystemHibernate, 0, TRUE);
+	}
+
+	else if (stricmp(argv[1], "shutdown") == 0 || stricmp(argv[1], "sh") == 0)
+	{
+		NtInitiatePowerAction(PowerActionShutdown, PowerSystemShutdown, 0, TRUE);
+	}
+
+	else if (stricmp(argv[1], "reboot") == 0 || stricmp(argv[1], "re") == 0)
+	{
+		NtInitiatePowerAction(PowerActionShutdownReset, PowerSystemWorking, 0, TRUE);
+	}
+
+	else if (stricmp(argv[1], "shutdownoff") == 0 || stricmp(argv[1], "so") == 0)
+	{
+		NtInitiatePowerAction(PowerActionShutdownOff, PowerSystemShutdown, 0, TRUE);
 	}
 }
 
@@ -22,9 +39,7 @@ int main(int argc, char* argv[])
 {
 	GetPrivilege(SE_SHUTDOWN_NAME);
 
-	paropt(argc, argv);
-
-	power(reco1,reco2);
+	power(argc, argv);
 
 	return 0;
 }
